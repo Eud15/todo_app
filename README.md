28e5785772e12c7a63916efa302ef3efaab43bd4
Eud15 <aboutaeudoxie@gmail.com>
2022-08-15 22:30:17 +0100

    Mon premier prjet en python

---
+++ README.md
@@ -0,0 +1,33 @@
+echo "# todo_app" >> README.md
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[### Development Setup
+
+First, [install Flask](http://flask.pocoo.org/docs/1.0/installation/#install-flask) if you haven't already.
+
+  ```
+  $ cd ~
+  $ sudo pip3 install Flask
+  ```
+
+To start and run the local development server,
+
+1. Initialize and activate a virtualenv:
+  ```
+  $ cd YOUR_PROJECT_DIRECTORY_PATH/
+  $ virtualenv --no-site-packages env
+  $ source env/bin/activate
+  ```
+
+2. Install the dependencies:
+  ```
+  $ pip install -r requirements.txt
+  ```
+
+3. Run the development server:
+  ```
+  $ export FLASK_APP=myapp
+  $ export FLASK_ENV=development # enables debug mode
+  $ python3 app.py
+  ```
+
+4. Navigate to Home page [http://localhost:5000](http://localhost:5000)
+
---
+++ app.py
@@ -0,0 +1,80 @@
+from crypt import methods
+from distutils.log import error
+from os import abort
+from flask import Flask, jsonify, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:eudo@localhost:5432/todoapp'
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    __tablename__ = 'todose'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False)
+
+    def __repr__(self):
+        return f'<Todo {self.id} {self.description}>'
+
+db.create_all()
+
+@app.route('/todose/create', methods=['POST'])
+def create_todo():
+    error = False
+    body = {}
+    try:
+        description = request.get_json()['description']
+        todo = Todo(description=description)
+        db.session.add(todo)
+        db.session.commit();
+        body['description'] = todo.description
+
+    except:
+        error=True
+        db.session.rollback()
+        print(sys.exc_info())
+
+    finally:
+        db.session.close()
+    if error:
+        abort(400)
+    else:
+        return jsonify(body)
+
+
+@app.route('/todose/<todo_id>/set-completed', methods = ['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        print('completed', completed)
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
+
+@app.route('/todose/<todo_id>', methods=['DELETE'])
+def delete_todo(todo_id):
+    try:
+        Todo.query.filter_by(id=todo_id).delete()
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    
+    return jsonify({ 'success': True })
+    
+
+        
+@app.route('/')
+def index():
+    return render_template('index.html', data=Todo.query.order_by('id').all())
+
+
+    
